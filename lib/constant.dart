e2bde9723bc0298b734641147b925ea8e0c68aa3
Mohamed Abdallah <mohamedelsebeay>
2025-02-18 00:32:34 +0200

    edit app is done

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 
 final kNotesBox = "note_box";
 final kColorSheet = Colors.deepPurple;
+List<Color>kColors=[
+  Colors.red,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.deepPurple,
+];
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/cubit/notes_cubit/cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/colors_list_view.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.note});
-final NoteModel note;
+  final NoteModel note;
 
   @override
   State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
 }
 
 class _EditNoteViewBodyState extends State<EditNoteViewBody> {
-  String ?title,content;
+  String? title, content;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,9 +25,9 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
         children: [
           SizedBox(height: 60),
           CustomAppBar(
-            onPressed: (){
-              widget.note.title=title??widget.note.title;
-              widget.note.subtitle=content??widget.note.subtitle;
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subtitle = content ?? widget.note.subtitle;
               widget.note.save();
               BlocProvider.of<NotesCubit>(context).fetchAllNotes();
               Navigator.pop(context);
@@ -36,17 +38,71 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
           SizedBox(height: 50),
           CustomTextField(
             onChanged: (value) {
-              title=value;
+              title = value;
             },
-            hint: widget.note.title),
+            hint: widget.note.title,
+          ),
           SizedBox(height: 16),
           CustomTextField(
             onChanged: (value) {
-              content=value;
+              content = value;
             },
-            hint: widget.note.subtitle, maxlines: 5),
+            hint: widget.note.subtitle,
+            maxlines: 5,
+          ),
+          SizedBox(height: 16),
+EditNoteColorsList(
+  note: widget.note,
+),
         ],
       ),
     );
+  }
+}
+
+
+
+
+
+class EditNoteColorsList extends StatefulWidget {
+  const EditNoteColorsList({super.key, required this.note});
+final NoteModel note;
+  @override
+  State<EditNoteColorsList> createState() => _EditNoteColorsListState();
+}
+
+class _EditNoteColorsListState extends State<EditNoteColorsList> {
+late int currentIndex;
+@override
+  void initState() {
+    currentIndex=kColors.indexOf(Color(widget.note.color));
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 64,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: kColors.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex =index;
+                widget.note.color=kColors[index].value;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: kColors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  
   }
 }
